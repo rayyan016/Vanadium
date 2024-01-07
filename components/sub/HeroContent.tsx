cd2f93400cd7443ed7db256169b169800db04204
Rayyan <rayyanaslam16@gmail.com>
2024-01-08 03:00:51 +0530

    Fix typo in HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -34,7 +34,7 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Hey, I'm{" "}
+            Hey, I&apos;m{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               Perseus! <br />
             </span>

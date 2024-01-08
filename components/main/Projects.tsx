615c4d9f923436e2f7e13ed9f7601431040dcc41
Rayyan <rayyanaslam16@gmail.com>
2024-01-08 15:53:28 +0530

    Refactor Projects and ProjectCard components

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,10 +3,7 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
@@ -15,16 +12,19 @@ const Projects = () => {
           src="/Proj1.png"
           title="Nasa APOD"
           description="Web application that fetches the Astronomy Picture of the Day (APOD) from the NASA API and displays it using React and Tailwind CSS."
+          url="https://github.com/rayyan016/Oxygen"
         />
         <ProjectCard
           src="/Proj2.png"
           title="Facebook Frontend Clone"
-          description="A clone of Facebook login page frontend using Tailwind CSS."
+          description="A clone of Facebook login page frontend created in order to understand working of Tailwind CSS."
+          url="https://github.com/rayyan016/facebookFrontendClone"
         />
         <ProjectCard
           src="/Proj3.png"
           title="Space Themed Website"
           description="This project was created with the goal of learning about the working of Three.js and animations in the frontend. It also utilizes Tailwind CSS and TypeScript for enhanced styling and type safety."
+          url="https://github.com/rayyan016/Vanadium"
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { RxGithubLogo } from "react-icons/rx";
 
 interface Props {
   src: string;
   title: string;
   description: string;
+  url: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, url }: Props) => {
   return (
-    <div className="relative md:w-1/3 overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative md:w-1/3 overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-20">
       <Image
         src={src}
         alt={title}
         width={1000}
-        height={1000} 
+        height={1000}
         className="w-full object-contain"
       />
-
       <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <div className="flex justify-between mb-4">
+          <p className="text-2xl font-semibold text-white">{title}</p>
+          <Link
+            href={url}
+            className="text-white hover:text-gray-300 text-3xl"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <RxGithubLogo />
+          </Link>
+        </div>
         <p className="mt-2 text-gray-300">{description}</p>
       </div>
     </div>

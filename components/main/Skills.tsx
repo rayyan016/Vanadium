558d55dc7e61ecb0bf7c47639f039695b0f7f9a0
Rayyan <rayyanaslam16@gmail.com>
2024-01-08 15:22:01 +0530

    Update skill imports

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
 
-import { Backend_skill, Frontend_skill, Skill_data } from "@/constants";
+import { Backend_skill, Frontend_skill, Languages } from "@/constants";
 
 const Skills = () => {
   return (
@@ -13,18 +13,6 @@ const Skills = () => {
       style={{ transform: "scale(0.9" }}
     >
       <SkillText />
-
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Frontend_skill.map((image, index) => (
@@ -48,6 +36,19 @@ const Skills = () => {
           />
         ))}
       </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Languages.map((image, index) => (
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div>
+
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
